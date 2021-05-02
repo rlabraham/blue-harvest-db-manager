4ade856914e978ac06e4fa4bb851c00f9c6c40b8
Lee Abraham <richard.lee.abraham@gmail.com>
2021-05-01 22:51:29 -0500

    Updated user table

--- db-manager/sql-scripts/0001.sql
+++ db-manager/sql-scripts/0001.sql
@@ -1,10 +1,10 @@
 CREATE TABLE user (
-  userID bigint NOT NULL PRIMARY KEY,
-  givenName varchar(25),
-  familyName varchar(25),
-  middleName varchar(25),
+  userid bigint NOT NULL PRIMARY KEY,
+  givenname varchar(25),
+  familyname varchar(25),
+  middlename varchar(25),
   salutations varchar(25),
-  dateOfBirth date,
+  dateofbirth date,
   description text,
-  gender enum('male', 'female', 'trans-female', 'trans-male', 'non-binary')
+  gender enum('male', 'female', 'transfemale', 'transmale', 'nonbinary')
 );
